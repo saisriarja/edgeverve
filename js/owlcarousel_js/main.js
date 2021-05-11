f10cafc87f147880a06414ce26ca01617c3c268d
saisriarja@gmail.com <saisriarja@gmail.com>
2021-05-11 16:17:36 +0530

    4:17 push

--- js/owlcarousel_js/main.js
+++ js/owlcarousel_js/main.js
@@ -1,43 +1,44 @@
 $(document).ready(function () {
-    $('.bannerr-carousell').owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
-    $('.counter').each(function() {
-        var $this = $(this),
-            countTo = $this.attr('data-count');
-        
-        $({ countNum: $this.text()}).animate({
-          countNum: countTo
+  $('.bannerr-carousell').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+  $('.counter').each(function () {
+    var $this = $(this),
+      countTo = $this.attr('data-count');
+
+    $({ countNum: $this.text() }).animate({
+      countNum: countTo
+    },
+
+      {
+
+        duration: 3000,
+        easing: 'linear',
+        step: function () {
+          $this.text(Math.floor(this.countNum));
         },
-      
-        {
-      
-          duration: 3000,
-          easing:'linear',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-      
-        });  
-        
-        
-      
+        complete: function () {
+          $this.text(this.countNum);
+          //alert('finished');
+        }
+
       });
+  });
+  $('.header-search-wrapper .search-main').click(function () {
+    $('.search-form-main').toggleClass('active-search');
+    $('.search-form-main .search-field').focus();
+  });
 });
